858ce6d6f852696c9bb484626cab2c3894f54427
Oasis300 <82212761+Oasis300@users.noreply.github.com>
2022-06-25 13:11:40 +0200

    Update Display.java

--- src/main/java/com/github/trqhxrd/untitledgame/Display.java
+++ src/main/java/com/github/trqhxrd/untitledgame/Display.java
@@ -1,4 +1,23 @@
 package com.github.trqhxrd.untitledgame;
 
+import java.awt.*;
+
 public class Display {
-}
+    private int höhe, länge;
+   private String Name;
+
+   private static Window Display= null;
+
+    private Display() {
+        this.länge = 1920;
+        this.höhe = 1080;
+        this.Name = "DER NAME";
+    }
+
+    public static Window get() {
+        if (Window.Display == null) {
+            Window.Dispaly = new Window();
+        }
+
+
+    }
