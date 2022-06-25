6410d6f346ebb467776b14cd7aae66ca296ff8df
trqhxrd <trqhxrd@github.com>
2022-06-25 12:14:23 +0200

    Added LWJGL.

---
+++ src/main/java/Display.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Display extends JFrame {
+
+    private Canvas canvas;
+
+    public Display(int länge, int höhe) {
+
+    }
+}
+//ok mach ich
--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,9 @@
+import org.lwjgl.glfw.GLFW;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println(Constants.TEXT);
+
     }
 }
