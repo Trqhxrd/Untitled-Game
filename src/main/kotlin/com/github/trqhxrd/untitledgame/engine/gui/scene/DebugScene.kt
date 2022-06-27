6aca161622cba5636ec30259917f9449a34747aa
Trqhxrd <53657159+Trqhxrd@users.noreply.github.com>
2022-06-27 20:47:57 +0200

    Removed comments.

--- src/main/kotlin/com/github/trqhxrd/untitledgame/engine/gui/scene/DebugScene.kt
+++ src/main/kotlin/com/github/trqhxrd/untitledgame/engine/gui/scene/DebugScene.kt
@@ -39,40 +39,27 @@ class DebugScene(window: Window) : Scene(window, background = Color.BLACK) {
     private var fragmentID: Int = 0
     private var shaderProgram: Int = 0
 
-    private val vertexArray = floatArrayOf( // position               // color
-        0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,  // Bottom right 0
-        -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,  // Top left     1
-        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,  // Top right    2
+    private val vertexArray = floatArrayOf(
+        0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+        -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,
         -0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f
     )
 
-    // IMPORTANT: Must be in counter-clockwise order
-    private val elementArray = intArrayOf( /*
-                    x        x
-                    x        x
-             */
-        2, 1, 0,  // Top right triangle
-        0, 1, 3 // bottom left triangle
+    private val elementArray = intArrayOf(
+        2, 1, 0,
+        0, 1, 3
     )
 
     private var vaoID = 0
     private var vboID: Int = 0
     private var eboID: Int = 0
 
-    fun LevelEditorScene() {}
-
     init {
-        // ============================================================
-        // Compile and link shaders
-        // ============================================================
-
-        // First load and compile the vertex shader
         vertexID = glCreateShader(GL_VERTEX_SHADER)
-        // Pass the shader source to the GPU
         glShaderSource(vertexID, vertexShaderSrc)
         glCompileShader(vertexID)
 
-        // Check for errors in compilation
         var success: Int = glGetShaderi(vertexID, GL_COMPILE_STATUS)
         if (success == GL_FALSE) {
             val len: Int = glGetShaderi(vertexID, GL_INFO_LOG_LENGTH)
@@ -81,13 +68,10 @@ class DebugScene(window: Window) : Scene(window, background = Color.BLACK) {
             assert(false) { "" }
         }
 
-        // First load and compile the vertex shader
         fragmentID = glCreateShader(GL_FRAGMENT_SHADER)
-        // Pass the shader source to the GPU
         glShaderSource(fragmentID, fragmentShaderSrc)
         glCompileShader(fragmentID)
 
-        // Check for errors in compilation
         success = glGetShaderi(fragmentID, GL_COMPILE_STATUS)
         if (success == GL_FALSE) {
             val len: Int = glGetShaderi(fragmentID, GL_INFO_LOG_LENGTH)
@@ -96,13 +80,11 @@ class DebugScene(window: Window) : Scene(window, background = Color.BLACK) {
             assert(false) { "" }
         }
 
-        // Link shaders and check for errors
         shaderProgram = glCreateProgram()
         glAttachShader(shaderProgram, vertexID)
         glAttachShader(shaderProgram, fragmentID)
         glLinkProgram(shaderProgram)
 
-        // Check for linking errors
         success = glGetProgrami(shaderProgram, GL_LINK_STATUS)
         if (success == GL_FALSE) {
             val len: Int = glGetProgrami(shaderProgram, GL_INFO_LOG_LENGTH)
@@ -111,29 +93,22 @@ class DebugScene(window: Window) : Scene(window, background = Color.BLACK) {
             assert(false) { "" }
         }
 
-        // ============================================================
-        // Generate VAO, VBO, and EBO buffer objects, and send to GPU
-        // ============================================================
         vaoID = glGenVertexArrays()
         glBindVertexArray(vaoID)
 
-        // Create a float buffer of vertices
         val vertexBuffer = BufferUtils.createFloatBuffer(vertexArray.size)
         vertexBuffer.put(vertexArray).flip()
 
-        // Create VBO upload the vertex buffer
         vboID = glGenBuffers()
         glBindBuffer(GL_ARRAY_BUFFER, vboID)
         glBufferData(GL_ARRAY_BUFFER, vertexBuffer, GL_STATIC_DRAW)
 
-        // Create the indices and upload
         val elementBuffer = BufferUtils.createIntBuffer(elementArray.size)
         elementBuffer.put(elementArray).flip()
         eboID = glGenBuffers()
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboID)
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, elementBuffer, GL_STATIC_DRAW)
 
-        // Add the vertex attribute pointers
         val positionsSize = 3
         val colorSize = 4
         val floatSizeBytes = 4
@@ -144,18 +119,14 @@ class DebugScene(window: Window) : Scene(window, background = Color.BLACK) {
         glEnableVertexAttribArray(1)
     }
 
-    override fun update(dt: Float) {
-        // Bind shader program
+    override fun update(dTime: Float) {
         glUseProgram(shaderProgram)
-        // Bind the VAO that we're using
         glBindVertexArray(vaoID)
 
-        // Enable the vertex attribute pointers
         glEnableVertexAttribArray(0)
         glEnableVertexAttribArray(1)
         glDrawElements(GL_TRIANGLES, elementArray.size, GL_UNSIGNED_INT, 0)
 
-        // Unbind everything
         glDisableVertexAttribArray(0)
         glDisableVertexAttribArray(1)
         glBindVertexArray(0)
