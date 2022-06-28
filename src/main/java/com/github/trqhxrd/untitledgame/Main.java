4c9b268e8bf09cd9217ff9b9994d21933f22f60b
Oasis300 <82212761+Oasis300@users.noreply.github.com>
2022-06-28 20:13:08 +0200

    Update Main.java

--- src/main/java/com/github/trqhxrd/untitledgame/Main.java
+++ src/main/java/com/github/trqhxrd/untitledgame/Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static void main(String[] args) {
         Core.INSTANCE.init();
 
-        logger.info("Hello World!");
+        logger.info("START");
 
         // Render r = new Render();
 
