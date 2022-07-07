92db96cded85ba4cd7ebaa26be102ed80d18ec3a
Trqhxrd <53657159+Trqhxrd@users.noreply.github.com>
2022-07-07 13:37:24 +0200

    Added fancy ass grid of squares because I can.

--- src/main/kotlin/com/github/trqhxrd/untitledgame/engine/gui/window/DebugScene.kt
+++ src/main/kotlin/com/github/trqhxrd/untitledgame/engine/gui/window/DebugScene.kt
@@ -1,11 +1,13 @@
 package com.github.trqhxrd.untitledgame.engine.gui.window
 
 import com.github.trqhxrd.untitledgame.engine.gui.util.Color
+import com.github.trqhxrd.untitledgame.engine.gui.util.Time
 import com.github.trqhxrd.untitledgame.engine.objects.GameObject
 import com.github.trqhxrd.untitledgame.engine.objects.components.SpriteRenderer
-import kotlin.math.sin
 
 class DebugScene : Scene("Debug Scene!", background = Color.BLACK) {
+
+    private var last = 0.0
 
     override fun init(window: Window) {
         super.init(window)
@@ -16,10 +18,16 @@ class DebugScene : Scene("Debug Scene!", background = Color.BLACK) {
 
         for (x in 0..99) {
             for (y in 0..99) {
-                val obj = GameObject("Object", x * 8 + 8, y * 8 + 8, 4, 4)
+                val obj = GameObject("Object", x * 5 + 8, y * 5 + 8, 4, 4)
                 obj.add(SpriteRenderer(Color(x / 100f, y / 100f, 0f, 1f)))
                 this.addObject(obj)
             }
         }
+    }
+
+    override fun preRender() {
+        val dTime = last / Time.now()
+        this.last = Time.now()
+        this.camera.x -= dTime.toFloat()/5
     }
 }
